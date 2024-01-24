1618ba373cb195e7a76731f32724a0a8512ad6e4
Timur Khusnutdinov <t.m.khusnutdinov@gmail.com>
2024-01-24 20:50:39 +0200

    First commit

---
+++ src/main/java/com/example/WorkTogetherApp/WorkTogetherAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.WorkTogetherApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkTogetherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkTogetherAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WorkTogetherApp/WorkTogetherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WorkTogetherApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkTogetherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
